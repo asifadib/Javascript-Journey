de68d58cd0265b473f7ce08a3aac53dce189840e
Asif Adib <asifadib780@gmail.com>
2021-03-01 15:05:16 +0600

    meta data update

--- theme.config.js
+++ theme.config.js
@@ -14,15 +14,15 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the next site builder" />
-      <meta name="og:description" content="Nextra: the next site builder" />
+      <meta name="description" content="জাভাস্ক্রিপ্ট জার্নি – সম্পূর্ণ ফ্রিতে বাংলায় জাভাস্ক্রিপ্ট শিখুন!" />
+      <meta name="og:description" content="জাভাস্ক্রিপ্ট জার্নি – সম্পূর্ণ ফ্রিতে বাংলায় জাভাস্ক্রিপ্ট শিখুন!" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
       <meta name="twitter:site:domain" content="nextra.vercel.app" />
       <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
+      <meta name="og:title" content="জাভাস্ক্রিপ্ট জার্নি – সম্পূর্ণ ফ্রিতে বাংলায় জাভাস্ক্রিপ্ট শিখুন!" />
       <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="apple-mobile-web-app-title" content="জাভাস্ক্রিপ্ট জার্নি" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
       <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png"/>
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
